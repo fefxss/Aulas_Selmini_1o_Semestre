10e776ba10c2d18a30bf58520072542448ccdce9
Maria Fernanda Platz Silva Caldeira <70938740+fefxss@users.noreply.github.com>
2026-05-07 08:48:52 -0300

    Implement Main class to modify and print patient name

---
+++ Exercicio04/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Paciente p = new Paciente("Maria",1.59);
+        p.setNome("Maria Fernanda");
+        System.out.println(p.getNome());
+    }
+}
