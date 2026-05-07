9df0f342ae8d51c07af30fa6a1804f16b4fdc1c1
Maria Fernanda Platz Silva Caldeira <70938740+fefxss@users.noreply.github.com>
2026-05-07 08:55:38 -0300

    Create Main.java

---
+++ Projeto Conta/src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import conta.Conta;
+import corrente.ContaCorrente;
+import poupanca.ContaPoupanca;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ArrayList<Conta> lista = new ArrayList<>();
+        lista.add(new ContaPoupanca(1, 10000, 10));
+        lista.add(new ContaCorrente(2, 5000, 80000));
+
+        for(Conta conta : lista) {
+            conta.depositar(2000);
+            if (conta instanceof ContaPoupanca){
+                ((ContaPoupanca) conta).aplicarRendimento();
+            }
+        }
+
+
+    }
+}
